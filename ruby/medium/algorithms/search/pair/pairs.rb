8c7da0f8140cca1843e16241cca5a3397cae908b
Tiago Tiede <geschopf@gmail.com>
2016-11-27 23:06:06 +0100

    Search the Pairs - Search the pairs with sorting the array before
    https://www.hackerrank.com/challenges/pairs

---
+++ ruby/medium/algorithms/search/pair/pairs.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+def  pairs( a,k) 
+  number_diff = 0
+  aux = {}
+  a.sort.each do |n|
+     aux[n] = true
+     number_diff += 1 if aux[n - k]
+  end
+
+  number_diff
+end
+
+a = gets.strip.split(" ").map! {|i| i.to_i}
+b_size=a[0]
+k=a[1]
+b = gets.strip.split(" ").map! {|i| i.to_i}
+puts pairs(b,k)
+
